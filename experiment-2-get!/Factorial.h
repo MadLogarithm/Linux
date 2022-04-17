bd588597e8c2ab796ae081b257c37e55d2c7c495
MadLogarithm <74230760+MadLogarithm@users.noreply.github.com>
2022-04-17 13:14:24 +0800

    Add files via upload
    计算阶乘

---
+++ experiment-2-get!/Factorial.h
@@ -0,0 +1,31 @@
+#ifndef _FACTORIAL_H
+#define _FACTORIAL_H
+#include<bits/stdc++.h>
+#define LEN 10005 
+using namespace std;
+void GetFact(int n){
+	int ans[LEN];
+	ans[1]=1;
+	for(int i=2;i<LEN;i++) ans[i]=-1;
+	for(int i=1;i<=n;i++){
+		for(int j=1;ans[j]!=-1;j++) ans[j]*=i;
+		for(int j=1;ans[j]!=-1;j++){
+			if(ans[j]>=100000)
+				if(ans[j+1]==-1)
+				{
+					ans[j+1]=ans[j]/100000;
+					ans[j]%=100000;
+				}
+				else
+				{
+					ans[j+1]+=ans[j]/100000;
+					ans[j]%=100000;
+				}
+		}
+	}
+	int cnt=0;
+	for(int i=1;ans[i]!=-1;i++) cnt++;
+	for(int i=cnt;i>0;i--) cout<<ans[i];
+	return;
+}
+#endif //_FACTORIAL_H
---
+++ experiment-2-get!/main.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include"Factorial.h"
+using namespace std;
+int main()
+{
+	int n = 20;
+	GetFact(n);
+	return 0;
+}
